65afb0b0161a024cc1febd11e3dd6dfd6299d62a
birus <victor.olalla@gmail.com>
2014-02-03 07:24:28 -0500

    Commit con Bootstrap Integrado

--- BibliotecaVirtual/settings.py
+++ BibliotecaVirtual/settings.py
@@ -131,6 +131,11 @@ INSTALLED_APPS = (
     # 'django.contrib.admindocs',
 )
 
+from django.core.urlresolvers import reverse_lazy
+LOGIN_URL=reverse_lazy('login')
+LOGIN_REDIRECT_URL= reverse_lazy('login')
+LOGOUT_URL=reverse_lazy('logout')
+
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
--- apps/inicio/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render_to_response
-from django.views.generic import TemplateView
-
-class index(TemplateView):
-	template_name='inicio/index.html'
-
-
-class index2(TemplateView): #Hereda todo lo que tiene TemplateView
-	template_name='inicio/index2.html'
